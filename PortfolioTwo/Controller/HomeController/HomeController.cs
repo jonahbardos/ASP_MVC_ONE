89ddc81abe35deb37e24f9adfaf30bd8f9630b6b
jonahbardos <jonahbardos@gmail.com>
2019-03-24 20:11:11 -0700

    Portfolio Two

---
+++ PortfolioTwo/Controller/HomeController/HomeController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioTwo{
+    public class HomeController : Controller{
+        
+        [HttpGet("")]
+        public ViewResult Index(){
+            return View();
+        }
+
+        [HttpGet("projects")]
+        public ViewResult Projects(){
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public ViewResult Contact(){
+            return View();
+        }
+    }
+}
